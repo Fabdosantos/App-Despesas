f02219923788dc92ed5ed6a0a9b284ec789aca03
Fabdosantos <fabdosantos2009@hotmail.com>
2024-02-25 22:00:55 -0300

    Adiciona tela de adição da despesa

--- app.py
+++ app.py
@@ -2,7 +2,31 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = 'App Despesas'
-    page.update()
+
+    def add_installments_input(e):
+        if (dd.value == "Parcelada"):
+            input.visible = True
+            page.update()
+            return
+        input.visible = False
+        page.update()
+
+    input = ft.TextField(label="Quantidade de Parcelas", visible=False)
+    dd = ft.Dropdown(
+         label='Tipo',
+         on_change=add_installments_input,
+         options=[
+            ft.dropdown.Option('Unica'),
+            ft.dropdown.Option('Parcelada'),
+        ])
+
+    page.add(
+        ft.Text('Adicione uma despesa', theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM),
+        ft.TextField(label='Nome da Despesa'),
+        dd,
+        input,
+        ft.TextField(label='R$'),
+    )
     pass
 
 
