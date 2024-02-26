252d9e5ac2402096c6518b02ad00930bce0dea3c
Fabdosantos <fabdosantos2009@hotmail.com>
2024-02-26 08:05:05 -0300

    Botao adicionar uma despesa

--- app.py
+++ app.py
@@ -11,6 +11,19 @@ def main(page: ft.Page):
         input.visible = False
         page.update()
 
+    def add_expense(e):
+        expense = {
+            "name": name.value,
+            "type": dd.value,
+            "installments": input.value,
+            "price": price.value,
+        }
+
+        print(expense)
+        pass
+
+    name = ft.TextField(label='Nome da Despesa')
+    price = ft.TextField(label='Valor', prefix_text='R$')
     input = ft.TextField(label="Quantidade de Parcelas", visible=False)
     dd = ft.Dropdown(
          label='Tipo',
@@ -22,10 +35,11 @@ def main(page: ft.Page):
 
     page.add(
         ft.Text('Adicione uma despesa', theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM),
-        ft.TextField(label='Nome da Despesa'),
+        name,
         dd,
         input,
-        ft.TextField(label='R$'),
+        price,
+        ft.ElevatedButton(text='Adicionar', on_click=add_expense),
     )
     pass
 
