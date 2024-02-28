c35a9b8a1caed603605b888aa158f7f15245eaaa
Fabdosantos <fabdosantos2009@hotmail.com>
2024-02-27 21:05:11 -0300

    Adiciona formatação do campo price

--- app.py
+++ app.py
@@ -16,22 +16,26 @@ def main(page: ft.Page):
             "name": name.value,
             "type": dd.value,
             "installments": input.value,
-            "price": price.value,
+            "price": float(price.value),
         }
-
+        
         print(expense)
-        pass
+
 
     name = ft.TextField(label='Nome da Despesa')
-    price = ft.TextField(label='Valor', prefix_text='R$')
     input = ft.TextField(label="Quantidade de Parcelas", visible=False)
     dd = ft.Dropdown(
-         label='Tipo',
-         on_change=add_installments_input,
-         options=[
+        label='Tipo',
+         on_change=add_installments_input,options=[
             ft.dropdown.Option('Unica'),
             ft.dropdown.Option('Parcelada'),
-        ])
+        ]
+    )
+    price = ft.TextField(
+        label='Valor',
+        prefix_text= "R$",
+        input_filter=ft.InputFilter(r"\d+(\.\d{0,2})?")
+    )
 
     page.add(
         ft.Text('Adicione uma despesa', theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM),
@@ -41,7 +45,6 @@ def main(page: ft.Page):
         price,
         ft.ElevatedButton(text='Adicionar', on_click=add_expense),
     )
-    pass
 
 
 ft.app(target=main)
